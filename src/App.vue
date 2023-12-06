2b7dea719a87d9a4f9fa962fabf504fbc66757fb
JuniorDCoder <foryoungjuniorngu@gmail.com>
2023-12-07 00:59:58 +0100

    feat: Initial Project Setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninga Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" :isPlaying="isPlaying" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Block,
+    Results,
+  },
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() + 3000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime){
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+    user-select: none;
+  }
+  button{
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    font-size: 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    letter-spacing: 1px;
+    margin: 10px;
+  }
+  button[disabled]{
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,44 @@
+<template>
+    <p>Reaction Time - {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+    export default{
+        props:  {
+            score: Number
+        },
+        data(){
+            return {
+                rank: null
+            }
+        },
+        mounted(){
+            if(this.score < 100){
+                this.rank = "Ninja Finger";
+            }else if(this.score < 200){
+                this.rank = "Grasshopper Limps";
+            }else if(this.score < 300){
+                this.rank = "Beginner";
+            }else{
+                this.rank = "Noob";
+            }
+        },
+        methods: {
+        }
+    }
+
+</script>
+
+<style scoped>
+    p{
+        font-size: 20px;
+        color: #0faf87;
+        font-weight: bold;
+    }
+    .rank{
+        font-size: 1.4rem;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
